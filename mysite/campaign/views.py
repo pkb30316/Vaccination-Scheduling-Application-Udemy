2d0f8ab137e4baf28e05cdb7e2e4cba91b0e99b4
pkb <pkb30316@gmail.com>
2023-10-29 23:45:06 +0530

    Vaccination Campaign Views Added

--- mysite/campaign/views.py
+++ mysite/campaign/views.py
@@ -1,3 +1,51 @@
+from typing import Any
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from campaign.models import Campaign
+from vaccination.models import Vaccination
+from campaign.forms import CampaignForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class CampaignListView(LoginRequiredMixin, generic.ListView):
+    model = Campaign
+    template_name = "campaign/campaign-list.html"
+    paginate_by = 10
+    ordering = ["-id"]
+
+
+class CampaignDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Campaign
+    template_name = "campaign/campaign-detail.html"
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["registration"] = Vaccination.objects.filter(campaign=self.kwargs["pk"]).count()
+        return context
+
+
+class CampaignCreateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, generic.CreateView):
+    model = Campaign
+    form_class = CampaignForm
+    permission_required = ("campaign.add_campaign",)
+    template_name = "campaign/campaign-create.html"
+    success_message = "Campaign Created Successfully"
+    success_url = reverse_lazy("campaign:campaign-list")
+
+class CampaignUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, generic.UpdateView):
+    model = Campaign
+    form_class = CampaignForm
+    permission_required = ("campaign:change_campaign",)
+    template_name = "campaign/campaign-update.html"
+    success_message = "Campaign Updated Successfully"
+    success_url = reverse_lazy("campaign:campaign-list")
+
+class CampaignDeleteView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, generic.DeleteView):
+    model = Campaign
+    template_name = "campaign/campaign-delete.html"
+    permission_required = ("campaign.delete_campaign",)
+    success_message = "Campaign Deleted Successfully"
+    success_url = reverse_lazy("campaign:campaign-list")
+    
